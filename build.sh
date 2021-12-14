ede8235c8bc0d75c3215f476990763b4635ae567
Meinte Boersma <meinte.boersma@gmail.com>
2021-12-14 22:53:09 +0100

    add retrieval and compression of value sets

--- build.sh
+++ build.sh
@@ -1,5 +1,29 @@
+#!/bin/sh
+
 npm install
 mkdir -p tmp
+
+valueSets="country-2-codes.json \
+  disease-agent-targeted.json\
+  test-manf.json\
+  test-result.json\
+  test-type.json\
+  vaccine-mah-manf.json\
+  vaccine-medicinal-product.json\
+  vaccine-prophylaxis.json\
+"
+
+for vs in $valueSets; do
+  curl "https://raw.githubusercontent.com/ehn-dcc-development/ehn-dcc-valuesets/main/$vs" > $vs
+done
+
+jq --slurp 'map( { (.valueSetId): .valueSetValues|keys }) | add' $valueSets > src/valueSets.json
+
+for vs in $valueSets; do
+  rm $vs
+done
+
+echo "Compressed value sets."
 
 curl -X GET --header "Accept: application/json" https://verifier-api.coronacheck.nl/v4/dcbs/business_rules | jq --raw-output '.payload' | base64 --decode | jq '.' > tmp/all-rules.json
 echo "Downloaded rules."
