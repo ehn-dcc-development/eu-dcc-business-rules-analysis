5bf7ae36e9f0fc9824e3546624eb97f1580ff50c
Meinte Boersma <meinte.boersma@gmail.com>
2022-04-12 10:30:46 +0200

    also produce all rules in a format compatible with DCC Crosscheck

--- retrieve.sh
+++ retrieve.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir -p tmp
+rm -f tmp/*.json
 
 curl --silent -X GET --header "Accept: application/json" https://verifier-api.coronacheck.nl/v4/dcbs/value_sets | jq --raw-output '.payload' | base64 --decode | jq '.' > tmp/valueSets-uncompressed.json
 node dist/refData/compress-value-sets.js
@@ -12,6 +13,9 @@ echo "Downloaded rules."
 rm -rf per-country/*
 node dist/split-rules-per-country.js
 echo "Split rules up per country."
+
+jq 'group_by(.Country) | map({ key: .[0].Country, value: (.|sort_by(.Identifier)|map({ key: .Identifier, value: .})|from_entries) }) | from_entries' tmp/all-rules.json > tmp/dcc-crosscheck.json
+echo "Stored all rules in the format compliant with DCC Crosscheck."
 
 node dist/check-rules.js
 echo "Checked (validated) all rules."
