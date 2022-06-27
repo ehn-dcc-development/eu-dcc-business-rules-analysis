6116c39bd52dd1048811521bec784a38cf8d429e
Meinte Boersma <meinte.boersma@gmail.com>
2022-06-27 15:36:14 +0200

    Janssen -> Jcovden

--- src/refData/vaccine-data.ts
+++ src/refData/vaccine-data.ts
@@ -7,7 +7,7 @@ const vaccineId2PopularName: { [vaccineId: string]: string } = {
     "EU/1/20/1528": "Comirnaty (Pfizer/BioNTech)",
     "EU/1/20/1507": "Spikevax (Moderna)",
     "EU/1/21/1529": "Vaxzevria (AstraZeneca)",
-    "EU/1/20/1525": "Janssen",
+    "EU/1/20/1525": "Jcovden (Janssen)",
     "EU/1/21/1618": "Nuvaxovid",
     "NVX-CoV2373": "Nuvaxovid (deprecated encoding)"
 }
