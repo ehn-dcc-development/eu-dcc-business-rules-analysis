77921c664e225bc5593c4e06a9579a44f56cce77
Meinte Boersma <meinte.boersma@gmail.com>
2022-10-11 23:32:16 +0200

    download + analyse rules 20221011   + add Valneva as EMA-authorised vaccine

--- src/refData/vaccine-data.ts
+++ src/refData/vaccine-data.ts
@@ -9,12 +9,13 @@ const vaccineId2PopularName: { [vaccineId: string]: string } = {
     "EU/1/21/1529": "Vaxzevria (AstraZeneca)",
     "EU/1/20/1525": "Jcovden (Janssen)",
     "EU/1/21/1618": "Nuvaxovid",
-    "NVX-CoV2373": "Nuvaxovid (deprecated encoding)"
+    "NVX-CoV2373": "Nuvaxovid (deprecated encoding)",
+    "EU/1/21/1624": "Valneva"
 }
 
 export const vaccineIdToDisplayName = (vaccineId: string): string =>
     vaccineId2PopularName[vaccineId] || vaccineId
 
 export const isEMAAuthorised = (vaccineId: string): boolean =>
-    [ "EU/1/20/1528", "EU/1/20/1507", "EU/1/21/1529", "EU/1/20/1525", "EU/1/21/1618" ].indexOf(vaccineId) !== -1
+    [ "EU/1/20/1528", "EU/1/20/1507", "EU/1/21/1529", "EU/1/20/1525", "EU/1/21/1618", "EU/1/21/1624" ].indexOf(vaccineId) !== -1
 
